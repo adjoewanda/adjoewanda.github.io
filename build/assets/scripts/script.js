ae89dae562502da328b20634363b93a58363dd9e
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2019-11-16 17:27:50 -0500

    submitted version

--- build/assets/scripts/script.js
+++ build/assets/scripts/script.js
@@ -1,4 +1,42 @@
 $(function(){
-  var x = 5;
-  return x;  
+  /*Header*/
+  $("header").css("height", "85vh");
+
+  /*Footer*/
+  $("footer").css("height", "30vh");
+  
+  /*Main Navigation*/
+  $("nav.main-menu ul").hide();
+
+  $("#menuBtn").click( () => {
+    $("nav.main-menu ul").toggle("slow");
+    $("#menuTop").toggleClass("fx-overlay");
+  });
+      
+  let position = $(window).scrollTop(); 
+
+  $(window).scroll( () => {
+    let scroll = $(window).scrollTop();
+    if(scroll > position) {
+      $("#menuMain").fadeOut("slow");
+      $("#menuTop").css("background-color", "initial");      
+    } else {
+      $("#menuMain").fadeIn("slow");
+      let x = ($(document).width() < 992) ? "rgba(0,0,0,.8)" : "rgba(0,0,0,.4)";
+      $("#menuTop").css({"background-color": x});      
+    }
+    position = scroll;
+  });
+  
+
+  /*================
+   *== About Page ==
+   *================*/
+
+  /* Timeline */
+  let x = new Date();
+  let y = new Date(2020, 04, 31);
+  let z = (x <= y) ? "Present" : "May 2020";
+  $("#dateNow").html(z);
+
 });
