ee4d51ddafb9d19a3f574a8d4e142e1399cdff99
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2020-05-13 18:14:13 -0400

    updated links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -28,7 +28,7 @@ const Projects = () => {
 
   const projects = content.map(project => {
 
-    const { title, src } = project;
+    const { title, src, demoURL, githubURL } = project;
 
     return (
       <div className="project-container container">
@@ -39,8 +39,8 @@ const Projects = () => {
           <div className="col-lg-4">
             <h2>{title}</h2>
             <p>Project codename West4, the challenge for this school project is to create a calculator to compute the tip amounts and total based on user input, using functions, conditions, and arithmetic operators. The finished product is an online tip calculator built with a live feature that automatically displays and changes the total as you type. Made with HTML, CSS, Javascript.</p>
-            <a className="btn btn-dark" href="https://tonychuaco.github.io/west4" target="_blank rel=noopener" title="View Demo">View Demo <i className="material-icons">open_in_new</i></a>
-            <a className="btn btn-dark" href="https://github.com/tonychuaco/west4" target="_blank rel=noopener" title="Download Source">Get Source <i className="material-icons">open_in_new</i></a>      
+            <a className="btn btn-dark" href={demoURL} target="_blank rel=noopener" title="View Demo">View Demo <i className="material-icons">open_in_new</i></a>
+            <a className="btn btn-dark" href={githubURL} target="_blank rel=noopener" title="Download Source">Get Source <i className="material-icons">open_in_new</i></a>      
           </div>
         </div>    
       </div>
