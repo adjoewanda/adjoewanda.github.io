9a10b6d5477033f6c48715b112b053d75869f227
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2020-06-10 18:10:26 -0400

    Added ACC project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import acc from './../images/advanced-copy-center-thumbnail.jpg';
 import mtgfan from './../images/mtgfan-thumbnail.png';
 import elmhurst from './../images/elmhurst-thumbnail.png';
 import mytravel from './../images/mytravel-thumbnail.png';
@@ -9,20 +10,27 @@ const Projects = () => {
   const content = [
     {
       key: 0,
+      title: "Advanced Copy Center Website",
+      src: acc,
+      demoURL: "https://advancedcopycenter.web.app",
+      githubURL: null,
+      description: "This freelance project is for a local printing company in New York City. The primary objective of the project is to create a website where users can contact and submit their print job inquiries online. Made with JavaScript, React, CSS, HTML, Bootstrap, and Firebase."
+    }, {
+      key: 1,
       title: "MTG Fan",
       src: mtgfan,
       demoURL: "https://mtgfan.herokuapp.com/",
       githubURL: "https://github.com/tonychuaco/mtgfan/",
       description: "This passion project for a React coursework is a tribute to Magic The Gathering, one of my favorite trading card games growing up. \"As a fan, I wanted to share my passion for Magic and showcase their gorgeous artworks\". Made with JavaScript, React, CSS, HTML, and Bootstrap."
     }, {
-      key: 1,
+      key: 2,
       title: "Elmhurst",
       src: elmhurst,
       demoURL: "https://tonychuaco.github.io/elmhurst/",
       githubURL: "https://github.com/tonychuaco/elmhurst/",
       description: "The challenge for this school project is to develop a website that introduces Elmhurst neighborhood in New York City. Final project for course in Web Technology I: Building and Maintaining Web Sites. This project uses gulp.js built tool. Made with CSS, Bootstrap, HTML."
     }, {
-      key: 2,
+      key: 3,
       title: "MYtravel",
       src: mytravel,
       demoURL: "https://et712-dad.github.io/mytravel/",
@@ -45,8 +53,8 @@ const Projects = () => {
           <div className="col-lg-4">
             <h2>{title}</h2>
             <p>{description}</p>
-            <a className="btn btn-dark" href={demoURL} target="_blank rel=noopener" title="View Demo">View Demo <i className="material-icons">open_in_new</i></a>
-            <a className="btn btn-dark" href={githubURL} target="_blank rel=noopener" title="Download Source">Get Source <i className="material-icons">open_in_new</i></a>      
+            <a className="btn btn-dark" href={demoURL} target="_blank rel=noopener" title="View Site">View Site <i className="material-icons">open_in_new</i></a>
+            {(githubURL === null) ? "" : <a className="btn btn-dark" href={githubURL} target="_blank rel=noopener" title="View repo on GitHub">Go to GitHub <i className="material-icons">open_in_new</i></a>}      
           </div>
         </div>    
       </div>
