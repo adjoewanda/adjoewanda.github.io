87c117e9b798ae828377742a1d17c61c69f8ef4c
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2020-06-10 14:19:33 -0400

    fixed typo

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => (
         <h2>About <span className="text-muted">Me</span></h2>
       </div>
       <div className="col-md-9 col-lg-8 col-xl-7">
-        <p>I am based in New York City, US and a graduate from CUNY TechWorks one-year Program in Sofware Development.</p>
+        <p>I am based in New York City, US, and a graduate from CUNY TechWorks one-year Program in Software Development.</p>
         <p>In my downtime I like to learn foreign languages and different cultures. I'm currently tuning in on Rakuten Viki and Spotify to try to pick up Mandarin. My goal is to become a polyglot and speaks multiple languages in addition to JavaScript, so that I can someday travel the world in 80 days and meet new people.</p>
       </div>
     </div>
